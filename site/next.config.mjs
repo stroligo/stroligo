91e9c3e49b559e13e48ae3aa51780a35d2876e96
stroligo <gabrielstroligo@gmail.com>
2024-05-08 22:46:13 +0200

    Next Instalado

--- site/next.config.mjs
+++ site/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  distDir: 'dist',
+  basePath: '/stroligo/dist',
+};
 
 export default nextConfig;
