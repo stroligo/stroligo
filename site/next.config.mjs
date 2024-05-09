c41125e78b6fe640658d71c3469eba22792bb203
stroligo <gabrielstroligo@gmail.com>
2024-05-09 16:28:59 +0200

    Next Instalado

--- site/next.config.mjs
+++ site/next.config.mjs
@@ -1,9 +1,15 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  /*   output: 'export', */
-  /*   trailingSlash: true, */
-  /*   skipTrailingSlashRedirect: true, */
-  /*   distDir: 'dist', */
+  output: 'export',
+  /*  assetPrefix: './', */
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+  // Optional: Change the output directory `out` -> `dist`
+  /*  distDir: 'dist', */
 };
 
 export default nextConfig;
