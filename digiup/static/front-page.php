e3f8655c972d6569349de59defa7cb0d5c5612f4
stroligo <gabrielstroligo@gmail.com>
2024-05-29 00:19:47 +0200

    iniciando os trabalhos

--- digiup/static/front-page.php
+++ digiup/static/front-page.php
@@ -27,19 +27,19 @@
 
                     <p class="subtitle">Trusted by millions across the globe:</p>
                     <ul class="flex gap-2 md:gap-5">
-                        <li><img src="<?php tu(); ?>/assets/img/partners/amazon.svg"></li>
-                        <li><img src="<?php tu(); ?>/assets/img/partners/apple.svg"></li>
-                        <li><img src="<?php tu(); ?>/assets/img/partners/google.svg"></li>
-                        <li><img src="<?php tu(); ?>/assets/img/partners/notion.svg"></li>
-                        <li><img src="<?php tu(); ?>/assets/img/partners/spotify.svg"></li>
-                        <li><img src="<?php tu(); ?>/assets/img/partners/slack.svg"></li>
+                        <li><img src="./assets/img/partners/amazon.svg"></li>
+                        <li><img src="./assets/img/partners/apple.svg"></li>
+                        <li><img src="./assets/img/partners/google.svg"></li>
+                        <li><img src="./assets/img/partners/notion.svg"></li>
+                        <li><img src="./assets/img/partners/spotify.svg"></li>
+                        <li><img src="./assets/img/partners/slack.svg"></li>
 
                     </ul>
                 </div>
             </div>
             <div class="md:w-6/12 flex-1 hero-doctor">
                 <figure>
-                    <img src="<?php tu(); ?>/assets/img/hero/img_doctor.png" class=" ">
+                    <img src="./assets/img/hero/img_doctor.png" class=" ">
                 </figure>
             </div>
         </div>
@@ -94,7 +94,7 @@
             <!-- Graphics Circles -->
             <div class="graphic">
                 <figure>
-                    <img src="<?php tu(); ?>/assets/img/graphics/dots-hero.png">
+                    <img src="./assets/img/graphics/dots-hero.png">
                 </figure>
             </div>
         </div>
@@ -118,7 +118,7 @@
                         <div class="w-full md:w-7/12 card">
                             <div class="interna">
                                 <figure class="mb-2">
-                                    <img src="<?php tu(); ?>/assets/img/services/online_consultations.png">
+                                    <img src="./assets/img/services/online_consultations.png">
                                 </figure>
                                 <h5>Online Consultations</h5>
                                 <p class=" text-sm font-semibold text-digiup-gray-400">
@@ -129,7 +129,7 @@
                         <div class="w-full md:w-5/12 card">
                             <div class="interna">
                                 <figure class="mb-2">
-                                    <img src="<?php tu(); ?>/assets/img/services/booking_appointments.png">
+                                    <img src="./assets/img/services/booking_appointments.png">
                                 </figure>
                                 <h5>Booking Appointments</h5>
                                 <p class=" text-sm font-semibold text-digiup-gray-400">
@@ -142,7 +142,7 @@
                         <div class="w-full md:w-1/3 card">
                             <div class="interna">
                                 <figure class="mb-2">
-                                    <img src="<?php tu(); ?>/assets/img/services/prescriptions.png">
+                                    <img src="./assets/img/services/prescriptions.png">
                                 </figure>
                                 <h5>Prescriptions</h5>
                                 <p class=" text-sm font-semibold text-digiup-gray-400">
@@ -153,7 +153,7 @@
                         <div class="w-full md:w-1/3 card">
                             <div class="interna">
                                 <figure class="mb-2">
-                                    <img src="<?php tu(); ?>/assets/img/services/medical_notes.png">
+                                    <img src="./assets/img/services/medical_notes.png">
                                 </figure>
                                 <h5>Medical Notes</h5>
                                 <p class=" text-sm font-semibold text-digiup-gray-400">
@@ -164,7 +164,7 @@
                         <div class="w-full md:w-1/3 card">
                             <div class="interna">
                                 <figure class="mb-2">
-                                    <img src="<?php tu(); ?>/assets/img/services/medicine_refills.png">
+                                    <img src="./assets/img/services/medicine_refills.png">
                                 </figure>
                                 <h5>Medicine Refills</h5>
                                 <p class=" text-sm font-semibold text-digiup-gray-400">
@@ -177,12 +177,12 @@
                     <!-- Graphics Waves -->
                     <div class="graphic1">
                         <figure>
-                            <img src="<?php tu(); ?>/assets/img/graphics/wave1.png">
+                            <img src="./assets/img/graphics/wave1.png">
                         </figure>
                     </div>
                     <div class="graphic2">
                         <figure>
-                            <img src="<?php tu(); ?>/assets/img/graphics/wave2.png">
+                            <img src="./assets/img/graphics/wave2.png">
                         </figure>
                     </div>
                 </div>
@@ -203,7 +203,7 @@
                         <div class="flex flex-col md:flex-row gap-4 md:gap-14">
                             <div>
                                 <figure class="w-full md:w-[448px]">
-                                    <img src="<?php tu(); ?>/assets/img/story/figure1.png">
+                                    <img src="./assets/img/story/figure1.png">
                                 </figure>
                             </div>
                             <div>
@@ -267,7 +267,7 @@
 
                         <div>
                             <figure>
-                                <img src="<?php tu(); ?>/assets/img/platform/doctor_full.png" class=" ">
+                                <img src="./assets/img/platform/doctor_full.png" class=" ">
                             </figure>
                         </div>
                     </div>
@@ -297,7 +297,7 @@
                     <div class="w-full md:w-6/12 card">
                         <div class="interna ">
                             <figure>
-                                <img src="<?php tu(); ?>/assets/img/testimonials/linda.png">
+                                <img src="./assets/img/testimonials/linda.png">
                             </figure>
                             <div>
                                 <p class=" font-medium m-0 text-digiup-gray-400">“After my knee surgery, the convenience of online consultations made my recovery smoother than I could have imagined.”</p>
@@ -308,7 +308,7 @@
                     <div class="w-full md:w-6/12 card">
                         <div class="interna ">
                             <figure>
-                                <img src="<?php tu(); ?>/assets/img/testimonials/henry.png">
+                                <img src="./assets/img/testimonials/henry.png">
                             </figure>
                             <div>
                                 <p class=" font-medium m-0 text-digiup-gray-400">“Managing chronic conditions like diabetes requires a lot of vigilance, but the medicine refill system has simplified my life.”</p>
@@ -345,7 +345,7 @@
                     <div class="w-full md:w-6/12 card">
                         <div class="interna ">
                             <figure>
-                                <img src="<?php tu(); ?>/assets/img/testimonials/joshua.png">
+                                <img src="./assets/img/testimonials/joshua.png">
                             </figure>
                             <div>
                                 <p class=" font-medium m-0 text-digiup-gray-400">“The prescription refill system is a game-changer for managing my diabetes. It’s really efficient and completely hassle-free.”</p>
@@ -356,7 +356,7 @@
                     <div class="w-full md:w-6/12 card">
                         <div class="interna ">
                             <figure>
-                                <img src="<?php tu(); ?>/assets/img/testimonials/samantha.png">
+                                <img src="./assets/img/testimonials/samantha.png">
                             </figure>
                             <div>
                                 <p class=" font-medium m-0 text-digiup-gray-400">“Finding a doctor who really understands all of my health needs has never been easier. This platform has changed my life.”</p>
@@ -371,12 +371,12 @@
                 <!-- Graphics Waves -->
                 <div class="graphic1">
                     <figure>
-                        <img src="<?php tu(); ?>/assets/img/graphics/global.png">
+                        <img src="./assets/img/graphics/global.png">
                     </figure>
                 </div>
                 <div class="graphic2">
                     <figure>
-                        <img src="<?php tu(); ?>/assets/img/graphics/global.png">
+                        <img src="./assets/img/graphics/global.png">
                     </figure>
                 </div>
             </div>
@@ -419,7 +419,7 @@
                         </div>
                         <div class="doctor-big ">
                             <figure>
-                                <img src="<?php tu(); ?>/assets/img/masters/doctorA.png">
+                                <img src="./assets/img/masters/doctorA.png">
                             </figure>
                         </div>
                     </div>
@@ -443,19 +443,19 @@
                         </div>
                         <div class="doctor-big ">
                             <figure>
-                                <img src="<?php tu(); ?>/assets/img/masters/doctorB.png">
+                                <img src="./assets/img/masters/doctorB.png">
                             </figure>
                         </div>
                     </div>
                     <!-- Graphics Waves -->
                     <div class="graphic1">
                         <figure>
-                            <img src="<?php tu(); ?>/assets/img/graphics/dots-master.png">
+                            <img src="./assets/img/graphics/dots-master.png">
                         </figure>
                     </div>
                     <div class="graphic2">
                         <figure>
-                            <img src="<?php tu(); ?>/assets/img/graphics/dots-master.png">
+                            <img src="./assets/img/graphics/dots-master.png">
                         </figure>
                     </div>
 
